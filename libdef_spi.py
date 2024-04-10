a3505871c4f91cae36971ef4b755d9a93d6d0299
chystad <christhy@stud.ntnu.no>
2024-04-10 19:27:33 +0200

    Fjernet unødvendige linjer

--- libdef_spi.py
+++ libdef_spi.py
@@ -70,66 +70,6 @@ def spi_wakeup(cs_pin, cs_mode, spi_init_time):
     reset_and_unexport(cs_pin)
 
 """
-def main():
-    initialize_spidev_modules()
-
-    mosi_low(mosi_pin, mosi_mode)
-
-    spi_wakeup(cs_pin, cs_mode, spi_init_time)
-
-    spi = spidev.SpiDev()
-    spi.open(0, 0)      # SPI1 is connected to bus 2!
-
-    # Configure spi communication 
-    spi.max_speed_hz = max_speed_hz
-    spi.mode = mode
-
-    sleep(1)
-
-    try:
-        n = 0
-        while True:
-            # Construct a frame to send consisting of a data byte and control byte respectivly
-            # I = 0x49
-            if n % 30000 == 0:
-                msg = "I".encode()
-                print(msg)
-                frame_to_send = [0X49, 0xA5]
-            else:
-                frame_to_send = [0xFF, 0xFF]
-            
-            # print(f'sending: {frame_to_send}')
-
-            # Send frame and read response
-            response = spi.xfer3(frame_to_send)
-            
-
-            if len(response) == frame_length:
-                data_byte, control_byte = response
-                if control_byte != 255:
-                    print(f'Received data byte: {data_byte}, Control byte: {control_byte}')
-            else:
-                print('Unexpected response length')
-
-            if (n > (10 * max_speed_hz)):
-                break
-                #and (control_byte == 0xFF)
-            n = n + 1
-        
-        print(f'Exit condition met. Closing spi communication...')
-        #print(f'Control_byte in response indicate end of transmission. Closing spi communication...')
-        spi.close()
-
-
-
-    except KeyboardInterrupt:
-        print('Closing spi communication...')
-        spi.close()
-
-main()
-"""
-
-"""
 19:15
 C434
 """
