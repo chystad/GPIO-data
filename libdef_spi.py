da05e088e7b2d7b96c623c8096220c5f7a581b66
chystad <christhy@stud.ntnu.no>
2024-04-15 19:51:15 +0200

    Small changes to SPI functionality

--- libdef_spi.py
+++ libdef_spi.py
@@ -11,65 +11,52 @@ from libdef_gpio import reset_and_unexport
 #   spi_mode, 
 #   wakeup_time
 # ]
-SPI_COMM_SPECS = [0, 0, 2, 400000, 0, 0.1]
+SPI_COMM_SPECS = [0, 0, 2, 200000, 0, 0.1]
 
 
 # Brief: Loads all spidev modules if it hasn't been loaded yet
 def initialize_spidev_modules():
-    if not os.path.exists(f'/dev/spidev0.0'):
-        print('importing spi modules...')
-        try:
-            os.system('sudo modprobe spidev')
-            print('Loading spidev modules...')
-            sleep(5)
-        except:
-            print('failed to import spi modules')
-    else:
-        print('spi modules already imported')
+   if not os.path.exists(f'/dev/spidev0.0'):
+       print('importing spi modules...')
+       try:
+           os.system('sudo modprobe spidev')
+           print('Loading spidev modules...')
+           sleep(5)
+       except:
+           print('failed to import spi modules')
+   else:
+       print('spi modules already imported')
 
 
 # Brief: Holds mosi LOW until the magnetometer has booted. Necessary for ensuring that 
 # the chipset on the connector-stip selects SPI communication protocol
 def mosi_low(mosi_pin, mosi_mode):
-    print('Exporting MOSI pin...')
-   
-    if not os.path.exists(f'/sys/class/gpio/gpio{str(mosi_pin)}'):
-        export_and_set_mode(mosi_pin, mosi_mode)
+   print('Exporting MOSI pin...')
+  
+   if not os.path.exists(f'/sys/class/gpio/gpio{str(mosi_pin)}'):
+       export_and_set_mode(mosi_pin, mosi_mode)
 
-    sleep(0.05)
-    print('Setting MOSI to LOW. You can now connect the magnetometer')
-    os.system(f'echo 0 > /sys/class/gpio/gpio{str(mosi_pin)}/value')
+   sleep(0.05)
+   print('Setting MOSI to LOW. You can now connect the magnetometer')
+   os.system(f'echo 0 > /sys/class/gpio/gpio{str(mosi_pin)}/value')
 
-    while True:
-        try:
-            txt_in = input('Do you wish to proceed to SPI communication? (y/n)')
-            if txt_in == 'y':
-                break
-        except KeyboardInterrupt:
-            break
-       
-    print('Program exited, unexporting MOSI pin...')
-    reset_and_unexport(mosi_pin)
-    sleep(0.05)
+   while True:
+       try:
+           txt_in = input('Do you wish to proceed to SPI communication? (y/n)')
+           if txt_in == 'y':
+               break
+       except KeyboardInterrupt:
+           break
+      
+   print('Program exited, unexporting MOSI pin...')
+   reset_and_unexport(mosi_pin)
+   sleep(0.05)
 
 # Brief: Creates a falling edge on the CS-pin. This is used as a wakeup-signal to the magnetometer 
 # which will make it enter its "operational mode"
-def spi_wakeup(cs_pin, cs_mode, spi_init_time):
-    print('Exporting CS pin...')
-    
-    if not os.path.exists(f'/sys/class/gpio/gpio{str(cs_pin)}'):
-        export_and_set_mode(cs_pin, cs_mode)
-    
-    sleep(0.05)
-    print('Setting CS to LOW')
-    os.system(f'echo 0 > /sys/class/gpio/gpio{str(cs_pin)}/value')
-
-    sleep(spi_init_time)
-
-    print('Program exited, unexporting CS pin...')
-    reset_and_unexport(cs_pin)
-
-"""
-19:15
-C434
-"""
+def spi_wakeup(spi, spi_init_time):
+   frame_to_send = [0x0D, 0xA5] # It does not matter what signal is sent beacuse CS will go HIGH->Low during a transmission anyway
+   response = spi.xfer2(frame_to_send)
+   print(response)
+   print('spi awakened (Hopefully)')
+   sleep(spi_init_time)
--- spi.py
+++ spi.py
@@ -1,3 +1,22 @@
+# Krav for å få SPI til å fungere
+#   (OPPFYLT) Power up: "If a high logic level is detected on the UART-RX pin (MOSI), the UART will be activated. Otherwise, the SPI port will be the active serial interface"
+#
+#   (OPPFYLT) Wake up cmd: "Falling edge signal on the SPI-CS pin" + info: "A single "wake up" character will be discarded by the microprocessor"
+#   Denne vil bli oppfylt bare av å sende et hvilket som helst signal ettersom CS går fra høy til lav under en transmisjon. 
+#
+#   (OPPFYLT) MSB overføres først (Sjekket med signalet '10000011' og det stemmer)
+#
+#   (OPPFYLT) Anbefalte innstillinger: 200kHz bus, minst 200 mikro sek mellom rammene 
+#
+#   (OPPFYLT) "The CS has to be set and released for every 16-bit frame"
+
+# Idle pin levels:
+#   MOSI: LOW
+#   MISO: LOW (ser litt ustabil ut? flyt?)
+#   SCK:  LOW
+#   CS:   HIGH
+
+
 import spidev
 from time import sleep
 from libdef_spi import initialize_spidev_modules
@@ -20,15 +39,13 @@ mag_wakeup_time = SPI_COMM_SPECS[5]
 mosi_pin = JETSON_NX_SPI0_PIN_DEFS[0][1]
 mosi_mode = JETSON_NX_SPI0_PIN_DEFS[0][2]
 cs_pin = JETSON_NX_SPI0_PIN_DEFS[3][1]
-cs_mode = JETSON_NX_SPI0_PIN_DEFS[3][2]
+cs_mode = JETSON_NX_SPI0_PIN_DEFS[3][2]  
 
 
 def main():
    initialize_spidev_modules()
 
-   mosi_low(mosi_pin, mosi_mode)
-
-   spi_wakeup(cs_pin, cs_mode, mag_wakeup_time)
+   # mosi_low(mosi_pin, mosi_mode)
 
    # Configure spi communication 
    spi = spidev.SpiDev()
@@ -40,22 +57,56 @@ def main():
    sleep(1)
 
    try:
+       spi_wakeup(spi, mag_wakeup_time)
+       sleep(1)
+
        n = 0
        while True:
+           if n == 0:
+               frame_to_send = [0x49, 0xA5]
+           if n == 1:
+               frame_to_send = [0x0D, 0xFF]
+           else:
+               frame_to_send = [0xFF, 0xFF]
+
+           response = spi.xfer2(frame_to_send)
+
+           data_byte, control_byte = response
+           
+           if True:
+               print(f'Received data byte: {data_byte}, Control byte: {control_byte}')
+
+           sleep(0.00001)
+           n = n + 1
+
+           """
+           if (n % 2) == 0:
+               frame_to_send = [0x83, 0xA5]
+           else:
+               frame_to_send = [0x83, 0xA5]
+
+           response = spi.xfer2(frame_to_send)
+           
+           # With this delay, the time in between each frame is measured to be around 250us (over 200us is recommended)
+           sleep(0.00001)
+           """
+           """
            # Construct a frame to send consisting of a data byte and control byte respectivly
            # I = 0x49
-           if n % 30000 == 0:
+           if (n+1) % 30000 == 0:
                msg = "I".encode()
                print(msg)
                frame_to_send = [0X49, 0xA5]
+           elif False:
+               frame_to_send = [0x0D, 0xA5]
            else:
                frame_to_send = [0xFF, 0xFF]
            
            # print(f'sending: {frame_to_send}')
 
            # Send frame and read response
-           response = spi.xfer3(frame_to_send)
-         
+           response = spi.xfer2(frame_to_send)
+           
 
            if len(response) == frame_length:
                data_byte, control_byte = response
@@ -67,6 +118,9 @@ def main():
            if (n > (10 * max_speed_hz)):
                break
                #and (control_byte == 0xFF)
+           """
+           
+           sleep(0.00001)
            n = n + 1
        
        print(f'Exit condition met. Closing spi communication...')
